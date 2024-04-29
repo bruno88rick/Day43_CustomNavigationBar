e3345996a2cee86cbe73a282df7fbfa6963b9369
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-04-29 05:56:09 -0300

    Initial Commit

---
+++ Day43_CustomNavigationBar/Day43_CustomNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day43_CustomNavigationBarApp.swift
+//  Day43_CustomNavigationBar
+//
+//  Created by Bruno Oliveira on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Day43_CustomNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
